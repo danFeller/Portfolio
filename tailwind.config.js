7bcba68cb1d8be69551c80835eea7c48a36e3324
danFeller <danielfeller2001@gmail.com>
2023-12-12 22:34:10 -0600

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}", './public/index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
