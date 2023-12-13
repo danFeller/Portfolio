e7cdffef9c66669a001da228f5b8257d0ef23e0b
danFeller <danielfeller2001@gmail.com>
2023-12-13 10:22:17 -0600

    file creation setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import About from './components/About'
 import NavBar from './components/NavBar'
+import Contact from './components/Contact'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
 
 export default function App() {
   return (
-    <>
+    <main className='bg-slate-900 text-gray-400'>
       <NavBar />
-      <h1 className="text-9xl text-center font-bold ">
-        Hello world!
-      </h1>
-    </>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,24 @@
 const NavBar = () => {
     return(
-        <nav>
-            <h1 className="text-4xl font-bold p-2">Navigation Bar</h1>
-        </nav>
+        <header>
+            <nav className="flex gap-9 bg-gray-800 justify-between">
+                <div className="flex gap-6">
+                    <a href='#' className="text-2xl font-bold p-3 hover:text-gray-200">Daniel Feller</a>
+                    <li className="flex gap-4 my-auto p-3">
+                        <ul>
+                            <a href="#About" className="hover:text-gray-200 font-semibold">About</a>
+                        </ul>
+                        <ul>
+                            <a href="#Projects" className="hover:text-gray-200 font-semibold">Projects</a>
+                        </ul>
+                        <ul>
+                            <a href="#Skills" className="hover:text-gray-200 font-semibold">Skills</a>
+                        </ul>
+                    </li>
+                </div>
+                <a href="#Contact" className="hover:text-gray-200 font-semibold my-auto px-8">Contact</a>
+            </nav>
+        </header>
     )
 }
 
