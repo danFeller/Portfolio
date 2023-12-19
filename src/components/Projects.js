559675e07550bc03e79839359d7522c5900911e4
danFeller <danielfeller2001@gmail.com>
2023-12-19 17:30:03 -0600

    Portfolio v1 is finished

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,9 +14,9 @@ const Projects = () => {
         <section id="Projects" className=' m-10 w-full mx-auto px-0.5 xsm:max-w-screen-xsm sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl'>
              <h1 className="font-semibold text-white text-5xl text-center xsm:text-center sm:text-6xl sm:text-center lg:text-7xl">Past Projects</h1>
              <div className="my-10 flex flex-row flex-wrap gap-4 justify-center">
-                <Project title='SloFashion' description="Android Budgeting App intended to raise awareness about fast fashion's negative impact on the environment" icons={[Java, Android]}/>
-                <Project title='College Hub' description="Full Stack Javascript application for organizing and joining upcoming events within your campus community" icons={[React, JavaScript, CSS, MongoDB, Express]}/>
-                <Project title='Portfolio' description="This Portfolio was built on my own with React and TailwindCSS and hosted on Firebase" icons={[React, JavaScript, TailwindCSS, Firebase]}/>
+                <Project title='SloFashion' description="Android Budgeting App intended to raise awareness about fast fashion's negative impact on the environment" icons={[Java, Android]} github="https://github.com/danFeller/SloFashion"/>
+                <Project title='College Hub' description="Full Stack Javascript application for organizing and joining upcoming events within your campus community" icons={[React, JavaScript, CSS, MongoDB, Express]} github="https://github.com/danFeller/CollegeHub"/>
+                <Project title='Portfolio' description="This Portfolio was built on my own with React and TailwindCSS and hosted on Firebase" icons={[React, JavaScript, TailwindCSS, Firebase]} github="https://github.com/danFeller/Portfolio"/>
              </div>
         </section>
     )
