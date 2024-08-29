247cb8b61c353e8f72410009b19ae009b44b70e2
danFeller <danielfeller2001@gmail.com>
2024-08-29 08:51:59 -0500

    mobile responsive; hamburger button added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ const Contact = () => {
         <section id="Contact" className=' m-16 w-full mx-auto px-0.5 xsm:max-w-screen-xsm sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl'>
             <div className="flex flex-col gap-4 text-center items-center">
                 <h1 className="font-semibold text-white text-5xl sm:text-6xl lg:text-7xl">Contact Me</h1>
-                <p className="text-center w-96 2xsm:text-sm">Want to get in touch with me? You can message me on LinkedIn or directly email me. You can also check out my projects on Github here as well.</p>
+                <p className="text-center w-96 2xsm:text-sm sm:text-lg lg:text-xl">Want to get in touch with me? You can message me on LinkedIn or directly email me. You can also check out my projects on Github here as well.</p>
                 <div className="flex 2xsm:gap-2 xsm:gap-2 sm:text-2xl md:text-3xl md:flex-row sm:gap-2 md:gap-2 my-5">
                     <a href="mailto:danielfeller2001@gmail.com" target="_blank" rel="noopener noreferrer" className=" bg-rose-400 text-white hover:bg-rose-500 hover:scale-110 font-semibold my-auto mx-1 px-5 py-2 rounded-md shadow-lg transition duration-300 xsm:px-10">Gmail</a>
                     <a href="https://www.linkedin.com/in/danielfeller2001/" target="_blank" rel="noopener noreferrer" className= " bg-sky-400 text-white hover:bg-sky-500 hover:scale-110 font-semibold my-auto mx-1 px-5 py-2 rounded-md shadow-lg transition duration-300 xsm:px-10">LinkedIn</a>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,39 @@
+import { useState } from 'react';
+
 const NavBar = () => {
-    return(
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
         <header className="bg-gray-800 fixed top-0 left-0 right-0 bg-opacity-90 z-50">
-            <nav className="flex gap-9 justify-between w-full mx-auto px-0.5 xsm:max-w-screen-xsm sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
-                <div className="flex gap-6 opa">
-                    <a href='#' className="text-2xl text-white font-bold px-1 py-2 hover:text-gray-200 xsm:w-44 2xsm:w-20">Daniel Feller</a>
-                    <li className="flex gap-4 my-auto p-3">
-                        <ul>
-                            <a href="#Projects" className="hover:text-gray-200 font-semibold">Projects</a>
-                        </ul>
-                        <ul>
-                            <a href="#Skills" className="hover:text-gray-200 font-semibold">Skills</a>
-                        </ul>
-                    </li>
+            <nav className="flex justify-between items-center w-full mx-auto px-4 py-2 xsm:max-w-screen-xsm sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+                <a href='#' className=" hidden sm:flex text-2xl text-white font-bold px-1 py-2 hover:text-gray-200">Daniel Feller</a>
+                <div className="flex w-3/5 flex-row-reverse justify-between sm:w-auto ml-auto">
+                    <button onClick={toggleMenu} className="text-white focus:outline-none sm:hidden">
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </button>
+                    <div className={`flex-col text-center sm:flex-row sm:flex ${isOpen ? 'flex' : 'hidden'} sm:gap-9 gap-1`}>
+                        <li className="flex flex-col sm:flex-row gap-4 my-auto p-3">
+                            <ul>
+                                <a href="#Projects" className="hover:text-gray-200 font-semibold">Projects</a>
+                            </ul>
+                            <ul>
+                                <a href="#Skills" className="hover:text-gray-200 font-semibold">Skills</a>
+                            </ul>
+                        </li>
+                        <a href="#Contact" className="bg-green-400 text-white hover:bg-green-500 font-semibold my-2 mx-1 px-3 py-1 rounded-md shadow-lg transition duration-300">Contact</a>
+                    </div>
                 </div>
-                <a href="#Contact" className="bg-green-400 text-white hover:bg-green-500 font-semibold my-auto mx-1 px-3 py-1 rounded-md shadow-lg transition duration-300">Contact</a>
             </nav>
         </header>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
 
 // border border-t-red-600 border-l-transparent border-r-transparent border-b-transparent
